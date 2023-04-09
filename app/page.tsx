de734860c09d3fc9015d223415a5ed7bf604bf5f
pablo kitzberger <kitzberger.pablo@gmail.com>
2023-04-09 15:53:02 -0300

    CL: - Basic page structure - Nav / Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,105 +1,108 @@
 "use client";
-import Image from "next/image";
-import {Inter} from "next/font/google";
 import {Button} from "@/components/ui/button";
-import {useToast} from "@/hooks/use-toast";
-import {Toaster} from "@/components/ui/toaster";
+import {ChevronsUp, FilePlus2, Github, Linkedin, PlusCircleIcon} from "lucide-react";
+import {Sheet, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger} from "@/components/ui/sheet";
+import {Toggle} from "@/components/ui/toggle";
+import {Switch} from "@/components/ui/switch";
 
-const inter = Inter({subsets: ["latin"]});
+const documents = [
+	{
+		name: "test",
+	},
+	{
+		name: "test2",
+	},
+	{
+		name: "test2",
+	},
+	{
+		name: "test2",
+	},
+	{
+		name: "test2",
+	},
+];
 
 export default function Home() {
-	const {toast} = useToast();
-
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<Toaster />
-			<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-				<p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-					Get started by editing&nbsp;
-					<code className="font-mono font-bold">app/page.tsx</code>
-				</p>
-				<div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-					<a
-						className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-						href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-						target="_blank"
-						rel="noopener noreferrer">
-						By{" "}
-						<Image
-							src="/vercel.svg"
-							alt="Vercel Logo"
-							className="dark:invert"
-							width={100}
-							height={24}
-							priority
-						/>
-					</a>
+		<main className="flex h-full min-h-screen flex-col bg-gradient-to-r from-zinc-50 to-white transition duration-700 dark:bg-gray-900/75 dark:bg-none">
+			<nav className="flex items-center bg-indigo-100/75 px-8 py-4 text-black drop-shadow-md transition duration-700 dark:bg-gradient-to-tl dark:from-gray-800/90 dark:via-gray-900/90 dark:to-black/90 dark:shadow-2xl">
+				<div className="grow overflow-x-auto">
+					<ul className="flex flex-nowrap gap-4 overflow-x-auto scroll-smooth p-2">
+						{documents.map((doc) => (
+							<Toggle
+								key={doc.name}
+								size="sm">
+								{doc.name}
+							</Toggle>
+						))}
+					</ul>
+				</div>
+				<span className="flex-initial">
+					<Sheet>
+						<SheetTrigger asChild>
+							<Button size="round">
+								<PlusCircleIcon />
+							</Button>
+						</SheetTrigger>
+						<SheetContent
+							position="right"
+							size="lg">
+							<SheetHeader>
+								<SheetTitle>Create Doc</SheetTitle>
+								<SheetDescription>Create your doc.</SheetDescription>
+								<div className="">test</div>
+								<SheetFooter>
+									<SheetTrigger>
+										<Button type="submit">Exit</Button>
+									</SheetTrigger>
+								</SheetFooter>
+							</SheetHeader>
+						</SheetContent>
+					</Sheet>
+				</span>
+			</nav>
+
+			<div className="flex-grow">
+				<div className="relative h-full w-full flex-grow text-slate-300">
+					{documents && (
+						<div className="absolute left-0 top-0 m-4 flex flex-col items-center justify-center">
+							<ChevronsUp className="m-2 h-8" />
+							<div>Seleccioná un documento</div>
+						</div>
+					)}
+					<div className="absolute right-0 top-0 m-4 flex flex-col items-center justify-center">
+						<FilePlus2 className="m-2 h-8" />
+						<div>Crea un documento</div>
+					</div>
 				</div>
 			</div>
 
-			<div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-				<Image
-					className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-					src="/next.svg"
-					alt="Next.js Logo"
-					width={180}
-					height={37}
-					priority
-				/>
+			<div className="flex h-12 w-full items-center justify-start gap-4 justify-self-end bg-slate-800 px-8 text-white">
+				<a
+					href="https://github.com/pablo-kitz"
+					target="_blank"
+					rel="noopener noreferrer">
+					<Button size="sm">
+						<Github />
+					</Button>
+				</a>
+				<a
+					href="https://www.linkedin.com/in/pablo-kitzberger/"
+					target="_blank"
+					rel="noopener noreferrer">
+					<Button size="sm">
+						<Linkedin />
+					</Button>
+				</a>
+				<div className="ml-auto flex gap-2 px-2">
+					<Switch />
+					<Switch
+						variant="select"
+						shape="square"
+					/>
+				</div>
 			</div>
-
-			<div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-				<a
-					href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer">
-					<h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-						Docs <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
-					</h2>
-					<p className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}>Find in-depth information about Next.js features and API.</p>
-				</a>
-
-				<a
-					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-					target="_blank"
-					rel="noopener noreferrer">
-					<h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-						Learn <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
-					</h2>
-					<p className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-				</a>
-
-				<a
-					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer">
-					<h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-						Templates <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
-					</h2>
-					<p className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}>Explore the Next.js 13 playground.</p>
-				</a>
-
-				<a
-					href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer">
-					<h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-						Deploy <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
-					</h2>
-					<p className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
-				</a>
-			</div>
-
-			<Button
-				onClick={() => {
-					toast({title: "tostadita"});
-				}}>
-				Toast
-			</Button>
 		</main>
 	);
 }
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -17,6 +17,7 @@ const buttonVariants = cva("inline-flex items-center justify-center rounded-md t
 			default: "h-10 py-2 px-4",
 			sm: "h-9 px-2 rounded-md",
 			lg: "h-11 px-8 rounded-md",
+			round: "p-2 h-10 w-10 rounded-full",
 		},
 	},
 	defaultVariants: {
