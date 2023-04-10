b98a0709cad466dbf26191ed1655b5184a3afae6
pablo kitzberger <kitzberger.pablo@gmail.com>
2023-04-10 19:06:31 -0300

    CL: - Remove Api routes, future task

--- app/api/documents/route.ts
+++
@@ -1,9 +0,0 @@
-import {PrismaClient} from "@prisma/client";
-import {NextApiRequest, NextApiResponse} from "next";
-
-const prisma = new PrismaClient();
-
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-	const docs = await prisma.document.findMany();
-	return new Response("response");
-}
